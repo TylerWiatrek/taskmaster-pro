624d9336f3c8692d7cb30b586152a0061f43e226
Tyler Wiatrek <wiatrektyler@yahoo.com>
2021-11-24 18:52:27 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
-  auditTask(taskLi);
+  //auditTask(taskLi);
 
 
   // append to ul list on the page
@@ -131,16 +131,18 @@ $(".card .list-group").sortable( {
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass(".dropover")
+    $(this).addClass(".bottom-trash-drag")
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass(".dropover")
+    $(this).removeClass(".bottom-trash-drag")
   },
   over: function(event) {
-    console.log("over", event.target);
-  },
+    $(event.target).addClass(".dropover-active")
+    },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass(".dropover-active");
   },
   update: function(event) {
     var tempArr = [];
@@ -177,13 +179,13 @@ $("#trash").droppable( {
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
-    console.log("drop");
+    $(".bottom-trash").removeClass(".bottom-trash-active")
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass(".bottom-trash-active")
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass(".bottom-trash-active")
   }
 });
 
@@ -201,7 +203,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -255,4 +257,8 @@ var auditTask = function(taskEl) {
   
 };
 
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
